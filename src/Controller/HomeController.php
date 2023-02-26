04591ccafee447298a76137f9040d4b5b42e1967
h-uwimana <302211792@student.rocmondriaan.nl>
2023-02-26 17:19:40 +0100

    adding pics

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,19 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/bh', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
 
-    #[Route('show')]
+
+    #[Route('/')]
     public function show(ManagerRegistry $doctrine):Response
     {
         $products=$doctrine->getRepository(Product::class)->findAll();
-       return  $this->render("home/index.html.twig", ["products"=>$products,
-            'controller_name' => 'HomeController']);
+       return  $this->render("home/index.html.twig", ["products"=>$products]);
     }
 }
