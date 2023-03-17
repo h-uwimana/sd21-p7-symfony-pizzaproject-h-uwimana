9cd4a2be743468adcad52d18dd745e49a1bf6229
h-uwimana <302211792@student.rocmondriaan.nl>
2023-03-17 20:48:10 +0100

    done with the homepage

--- migrations/Version20230315115101.php
+++ migrations/Version20230315115101.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230311160816 extends AbstractMigration
+final class Version20230315115101 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20230311160816 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product DROP price');
+        $this->addSql('ALTER TABLE `order` ADD price NUMERIC(5, 2) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product ADD price DOUBLE PRECISION NOT NULL');
+        $this->addSql('ALTER TABLE `order` DROP price');
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use App\Form\CartType;
 use Doctrine\Common\Collections\Selectable;
 use App\Entity\Category;
 use App\Entity\Product;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,22 +17,64 @@ class HomeController extends AbstractController
 {
 
 
-    #[Route('/')]
-    public function show(ManagerRegistry $doctrine, ):Response
+    #[Route('/', name: "home")]
+    public function show(ManagerRegistry $doctrine,Request $request ):Response
     {
+        $session = $request->getSession();
+
+//        dd($session);
         $products=$doctrine->getRepository(Product::class)->findAll();
         $category=$doctrine->getRepository(Category::class)->findAll();
-//        dd($products);
-       return  $this->render("home/index.html.twig", ["products"=>$products, "category"=>$category]);
+
+        $task = new Order();
+//        dd($request);
+        $form = $this->createForm(CartType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            dd($form->getData());
+        }
+//            dd($form);
+       return  $this->renderform("home/index.html.twig", ["form"=>$form,"products"=>$products, "category"=>$category,
+           ]);
     }
 
 
-    #[Route('/category/{id}')]
-    public function getCategory(ManagerRegistry $doctrine,int $id):Response
+    #[Route('/category/{id}', name: 'category')]
+    public function getCategory(ManagerRegistry $doctrine,int $id, Request $request):Response
     {
+
+
+//        dd($session);
         $products=$doctrine->getRepository(Product::class)->findBy(array("category"=>$id));
         $category=$doctrine->getRepository(Category::class)->findAll();
-//        dd($products);
-        return  $this->render("home/index.html.twig", ["products"=>$products, "category"=>$category]);
+
+        $task = new Order();
+
+        $form = $this->createForm(CartType::class, $task);
+
+        $form = $this->createForm(CartType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            dd($form->getData());
+        }
+        return  $this->render("home/index.html.twig", ["products"=>$products, "category"=>$category,
+            "form"=>$form->createView()]);
+    }
+
+    #[Route('/addCart/', name: 'addCart')]
+    public function addCart(ManagerRegistry $doctrine,Request $request):Response    {
+        dd($request);
+        $form = $this->createForm(CartType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            dd($form->getData());
+        }
+        return new Response("hoi");
     }
 }
+
+
+
+
+
+
