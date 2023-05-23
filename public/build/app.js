7fcd42cc2c84150619f17c39bbf89a14cdd7c81e
h-uwimana <302211792@student.rocmondriaan.nl>
2023-05-23 18:04:44 +0200

    helemaal klaar

--- public/build/app.js
+++ public/build/app.js
@@ -282,7 +282,7 @@ $(document).ready(function () {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "app": () => (/* binding */ app)
+/* harmony export */   app: () => (/* binding */ app)
 /* harmony export */ });
 /* harmony import */ var _symfony_stimulus_bridge__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @symfony/stimulus-bridge */ "./node_modules/@symfony/stimulus-bridge/dist/index.js");
 
